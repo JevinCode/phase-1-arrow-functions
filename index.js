b78cecc1fadea3540e0624f28b7a385631d90786
Jevin <therealjevin@gmail.com>
2023-01-10 09:34:15 -0600

    defined functions with let

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-divide = (dividend, divisor) => dividend / divisor;
-square = x => x*x;
-add = (a,b) => a + b;
+let divide = (dividend, divisor) => dividend / divisor;
+let square = x => x*x;
+let add = (a,b) => a + b;
